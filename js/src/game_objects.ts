745b293b38599125dad2f46c20b408084e432ab9
ponito <jonathan.rudel@nord-com.net>
2021-12-30 22:57:37 +0100

    more apple fixes

--- js/src/game_objects.ts
+++ js/src/game_objects.ts
@@ -86,7 +86,8 @@ class Snake implements renderable {
 
                     let next = this.tail;
                     while (next !== null) {
-                        if (next.pos === newPos) {
+                        if (next.pos[0] == newPos[0] && next.pos[1] == newPos[1]) {
+                            console.log("blocked");
                             blocked = true;
                             next = null;
                         }

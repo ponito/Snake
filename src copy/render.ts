050a37a6e4134508325caa86d0ba9cca032b8cd8
cactus-man <64769435+Cactus-man@users.noreply.github.com>
2021-12-30 20:02:05 +0100

    Changes!

--- src copy/render.ts
+++ src copy/render.ts
@@ -3,9 +3,22 @@ const ctx = canvas.getContext('2d');
 const canvasDims = { width: 600, height: 600 };
 const aspectRatio = canvasDims.height / canvasDims.width;
 
-const GRID: Grid = {};
+const SHARED_STATE = {};
+const GRID: Grid = {
+    // TODO configuration
+    width: 15,
+    height: 15,
+    cell_width: canvas.width / 15,
+    cell_height: canvas.height / 15,
+};
 const TREASURES: Treasure[] = [];
 const SNAKES: renderable[] = [];
+
+function init() {
+    fetch("config.json").then(() => {
+
+    })
+};
 
 const theatorFit = window.onresize = () => {
     let dims = {
@@ -24,13 +37,25 @@ const theatorFit = window.onresize = () => {
     }
 }
 
-const renderBackground = () => {};
+function renderBackground() {
+    draw.color = 'seagreen';
+    draw.fill = false;
+    for (let x = 0; x < GRID.cell_width; x++) {
+        for (let y = 0; y < GRID.cell_height; y++) {
+            // Use image?
+            draw.Rectangle(GRID.cell_width, GRID.cell_height,
+                [x * GRID.cell_width, y * GRID.cell_height])
+        }
+    }
+    draw.fill = true;
+    ctx.stroke();
+};
 
-const renderObjects = () => {};
+const renderObjects = () => { };
 
 const renderPlayers = () => SNAKES.forEach(s => s.render());
 
-const renderControls = () => {};
+const renderControls = () => { };
 
 function updateFrames() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
